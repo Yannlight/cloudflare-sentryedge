9b7e93c842c1c2c4384a354278f4d89095464ee8
Andreas Mallek <amallek@users.noreply.github.com>
2025-06-15 16:48:30 +0200

    updated

--- scripts/curl-post-event.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -X POST https://sentryedge-worker.datargo.workers.dev/logs \
-  -H "Content-Type: application/json" \
-  -d '{"service": "backend", "level": "error", "message": "Something went wrong"}'
-  
